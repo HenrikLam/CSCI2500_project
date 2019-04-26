02881524072923a7a9d6eef8868d05ab29c57c9b
Shadman Choudhury <choudhs2@bxscience.edu>
2019-04-26 18:23:47 -0400

    the file we submit is supposed to be named p1.cpp, so i just added that instead of project.cpp

---
+++ p1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <string.h>
+#include "simulation.h"
+
+int main(int argc, char* argv[]){
+    bool test = false;
+	if (argc < 3) return EXIT_FAILURE;
+    // use -t to toggle testing
+    bool forwarding = !(strcmp(argv[1], "N") == 0);
+    std::ifstream input_file(argv[2]);
+    std::string instructions[10];
+    int instruction_count = 0;
+    if (input_file.is_open()){
+        while (getline(input_file, instructions[instruction_count])){
+            if (test){
+                std::cout << instructions[instruction_count] + "\n";
+            }
+            ++instruction_count;
+        }
+        input_file.close();
+    }
+    Simulation s(forwarding);
+    s.parseInstructions(instructions, instruction_count);
+    std::string str = (forwarding) ? "forwarding" : "no forwarding";
+    std::cout<<"START OF SIMULATION ("<< str <<")\n";
+    s.simulate();
+    std::cout << "----------------------------------------------------------------------------------\n" 
+                << "END OF SIMULATION\n\n";
+}
